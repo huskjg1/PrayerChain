0799bdd19577bab202d2d896f6aac0e26cc82701
Josiah <huskjg1@gmail.com>
2014-03-16 17:09:28 -0400

    Adding pagination to users page

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    name     "Josiah Husk"
-    email    "jhusk@frontier.com"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
     password "foobar"
     password_confirmation "foobar"
   end
