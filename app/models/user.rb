c85745ef9d7d7077431bfae48c5e6d6e40257278
huskjg1 <huskjg1@gmail.com>
2014-03-22 12:54:18 -0400

    Relationships between users and prayer requests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :prayer_requests
   before_save { self.email = email.downcase }
   before_create :create_remember_token
 
--- spec/models/prayer_request_spec.rb
+++ spec/models/prayer_request_spec.rb
@@ -2,16 +2,15 @@ require 'spec_helper'
 
 describe PrayerRequest do
   let(:user) { FactoryGirl.create(:user) }
-  before do
-    # This code is not idiomatically correct.
-    @prayer_request = PrayerRequest.new(content: "Lorem ipsum", user_id: user.id)
-  end
+	before { @prayer_request = user.prayer_requests.build(content: "Lorem ipsum") }
 
   subject { @prayer_request }
 
   it { should respond_to(:content) }
   it { should respond_to(:user_id) }
-
+  it { should respond_to(:user) }
+  its(:user) { should eq user }
+  
   describe "when user_id is not present" do
   	before { @prayer_request.user_id = nil }
   	it { should_not be_valid }
