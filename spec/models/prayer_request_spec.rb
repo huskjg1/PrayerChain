b455f700564b8eab63b6cded691d04a361aeb81b
Josiah <huskjg1@gmail.com>
2014-03-29 18:07:01 -0400

    Ensure prayer requests are correct length

--- spec/models/prayer_request_spec.rb
+++ spec/models/prayer_request_spec.rb
@@ -10,9 +10,19 @@ describe PrayerRequest do
   it { should respond_to(:user_id) }
   it { should respond_to(:user) }
   its(:user) { should eq user }
-  
+
   describe "when user_id is not present" do
   	before { @prayer_request.user_id = nil }
   	it { should_not be_valid }
+  end
+
+  describe "with blank content" do
+    before { @prayer_request.content = " " }
+    it { should_not be_valid }
+  end
+
+  describe "with content that is too long" do
+    before { @prayer_request.content = "a" * 2001 }
+    it { should_not be_valid }
   end
 end
